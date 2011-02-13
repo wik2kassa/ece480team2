37edd474fb89192438576e202785ac50185224bd
guitarman9182@gmail.com <guitarman9182@gmail.com@a65c081c-d945-0d18-fa3c-6faaeb5b76e5>
2011-02-13 06:03:59 +0000

    I added in the DBManager class which will be used to make queries against an external Apache sever running PHP over the internet with a SQL backend which I set up. This is still a work in progres but will be finshed as I have time.

---
+++ trunk/src/com/iDocent/DBManager.java
@@ -0,0 +1,32 @@
+package com.iDocent;
+
+import org.json.*;
+import org.apache.*;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/*
+ * This class is going to be the interface to use 
+ * in order to query the SQL database backend. It
+ * will be making HTTP Queries to an apache server
+ * running php which in turn will be returning its
+ * results in a JSON format for the phone to parse
+ */
+public class DBManager {
+
+	private HttpClient client;	
+	private String server;
+	
+	public DBManager(){
+		client = new DefaultHttpClient();
+	}
+	
+	public void Query(String MAC){
+		HttpPost post = new HttpPost("http://480team2.dyndns-server.com");
+		post.setEntity(new UrlEncodedFormEntity());
+	}
+
+}
