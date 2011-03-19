3aa0257ab20bc04f281c78287d33b8155cc9e659
ghauls8@gmail.com <ghauls8@gmail.com@a65c081c-d945-0d18-fa3c-6faaeb5b76e5>
2011-03-19 20:53:51 +0000

    Communication.

--- trunk/src/com/iDocent/Map.java
+++ trunk/src/com/iDocent/Map.java
@@ -7,20 +7,23 @@ public class Map extends GraphicsObject{
 	
 	private Vector<GraphicsObject> Children;
 	
+	private float posX;
+	private float posY;
+	
 	public Map(){
 		Children = new Vector<GraphicsObject>();
 		
-		Children.add(new Line(0,16.4f, 0,28.4f));
-		Children.add(new Line(0,16.4f, 12,16.4f));
-		Children.add(new Line(0,28.4f, 12,28.4f));
-		Children.add(new Line(12,0, 12,16.4f));
-		Children.add(new Line(24.5f,0, 24.5f,16.4f));
-		Children.add(new Line(24.5f,16.4f, 143.5f,16.4f));
-		Children.add(new Line(24.5f,28.4f, 120,28.4f));
-		Children.add(new Line(143.5f,16.4f, 143.5f,28.4f));
-		Children.add(new Line(132,28.4f, 143.5f,28.4f));
-		Children.add(new Line(120,28.4f, 120,153.4f));
-		Children.add(new Line(132,28.4f, 132,153.4f));
+		Children.add(new Line(0,-16.4f, 0,-28.4f));
+		Children.add(new Line(0,-16.4f, 12,-16.4f));
+		Children.add(new Line(0,-28.4f, 12,-28.4f));
+		Children.add(new Line(12,0, 12,-16.4f));
+		Children.add(new Line(24.5f,0, 24.5f,-16.4f));
+		Children.add(new Line(24.5f,-16.4f, 143.5f,-16.4f));
+		Children.add(new Line(24.5f,-28.4f, 120,-28.4f));
+		Children.add(new Line(143.5f,-16.4f, 143.5f,-28.4f));
+		Children.add(new Line(132,-28.4f, 143.5f,-28.4f));
+		Children.add(new Line(120,-28.4f, 120,-153.4f));
+		Children.add(new Line(132,-28.4f, 132,-153.4f));
 	}
 
 	@Override
@@ -29,6 +32,12 @@ public class Map extends GraphicsObject{
 		for (GraphicsObject i:Children){
 			i.Draw(GL);
 		}
+	}
+
+	public void UpdateLoction(float x, float y) {
+		posX = x;
+		posY = y;
+		
 	}
 
 }
--- trunk/src/com/iDocent/ScanTask.java
+++ trunk/src/com/iDocent/ScanTask.java
@@ -14,21 +14,16 @@ import android.widget.TextView;
 
 //The task to be performed by the timer
 public class ScanTask extends TimerTask{
-    WeakReference<TextView> textView;
     WifiManager wifi;
     WeightedScanFactory wsFactory;
+    iDocent miD;
     
     Handler handler = new Handler();
 
-    public ScanTask(TextView text, WifiManager w) {
-        textView = new WeakReference<TextView>(text);
+    public ScanTask(iDocent iD, WifiManager w) {
+    	miD = iD;
         wifi = w;
         wsFactory = new WeightedScanFactory();
-    }
-    
-    public void SetTextView(TextView text)
-    {
-    	textView = new WeakReference<TextView>(text);
     }
 
     public void run() 
@@ -37,102 +32,54 @@ public class ScanTask extends TimerTask{
         {
             public void run() 
             {
-            	if(textView != null && textView.get() != null)
-            	{
-	                textView.get().setText("WiFi Scanner");
-	                
-	                //Makes sure that wifi is enabled and it can scan for access points
-	        		if(wifi.isWifiEnabled())
-	        		{
-	        			int count = 0;
-	        			
-	        			//Keep track of all available access points in a list
-	        			List<WeightedScan> weightedScans = new LinkedList<WeightedScan>();
-	        			Map<String, WeightedScan> scannedAPs = new HashMap<String, WeightedScan>();
-	        			for(int i=0; i<16; i++)
-	        			{
-			        		if(wifi.startScan())
-			        		{			        			
-				        		// List available networks
-				        		List<ScanResult> scans = wifi.getScanResults();
-				        		
-				        		if(scans != null && !scans.isEmpty())
-				        		{			        			
-				        			//Go through the list of scans, give them a value from 1-20 based on strength, then
-				        			//create a weighted scan and save it in the list
-					        		for (ScanResult scan : scans) 
-					        		{
-					        	        int level = WifiManager.calculateSignalLevel(scan.level, 20);
-					        	        
-					        	        WeightedScan ws = scannedAPs.get(scan.BSSID);
-					        	        
-					        	        if(ws != null)
-					        	        {
-					        	        	ws.SetLevel(ws.GetLevel()+level);
-				        	        		count+=level;
-					        	        }
-					        	        else
-					        	        {
-						        			WeightedScan wScan = wsFactory.Create(scan.BSSID);
-						        			if(wScan != null)
-						        			{
-						        				wScan.SetLevel(level);
-						        				count+=level;
-							        			weightedScans.add(wScan);
-							        			scannedAPs.put(wScan.Name(), wScan);
-						        			}
-					        	        }
-					        	        
-					        	        //An alternate way that saves some memory but takes longer
-//					        	        boolean addNew = true;
-//					        	        for(WeightedScan s : weightedScans)
-//					        	        {
-//					        	        	if(s.Name().equals(scan.BSSID))
-//					        	        	{
-//					        	        		s.SetLevel(s.GetLevel()+level);
-//					        	        		count+=level;
-//					        	        		addNew = false;
-//					        	        	}
-//					        	        }
-//					        			
-//					        	        if(addNew)
-//					        	        {
-//						        			WeightedScan wScan = wsFactory.Create(scan.BSSID);
-//						        			if(wScan != null)
-//						        			{
-//						        				wScan.SetLevel(level);
-//						        				count+=level;
-//							        			weightedScans.add(wScan);
-//						        			}
-//					        	        }
-					        		}
-				        		}
-				        		else
-				        		{
-				        			textView.get().setText("WiFi Scanner\nScanning...");
-				        		}
-			        		}
-	        			}
-	        			
-		        		if(!weightedScans.isEmpty())//DEMO CODE - for display
-		        		{
-			        		textView.get().append("\n\n");
-			        		for (WeightedScan wscan : weightedScans)
-			        		{
-			        			textView.get().append(wscan.Name() + " " + wscan.GetLevel() + "/" + count + 
-			        					"\n" + wscan.GetPos().get(0) + " " + wscan.GetPos().get(1) + "\n");
-			        		}
+        		if(wifi.isWifiEnabled())
+        		{
+        			int count = 0;
+        			
+        			//Keep track of all available access points in a list
+        			List<WeightedScan> weightedScans = new LinkedList<WeightedScan>();
+        			Map<String, WeightedScan> scannedAPs = new HashMap<String, WeightedScan>();
+        			for(int i=0; i<16; i++)
+        			{
+		        		if(wifi.startScan())
+		        		{			        			
+			        		// List available networks
+			        		List<ScanResult> scans = wifi.getScanResults();
 			        		
+			        		if(scans != null && !scans.isEmpty())
+			        		{			        			
+			        			//Go through the list of scans, give them a value from 1-20 based on strength, then
+			        			//create a weighted scan and save it in the list
+				        		for (ScanResult scan : scans) 
+				        		{
+				        	        int level = WifiManager.calculateSignalLevel(scan.level, 20);
+				        	        
+				        	        WeightedScan ws = scannedAPs.get(scan.BSSID);
+				        	        
+				        	        if(ws != null)
+				        	        {
+				        	        	ws.SetLevel(ws.GetLevel()+level);
+			        	        		count+=level;
+				        	        }
+				        	        else
+				        	        {
+					        			WeightedScan wScan = wsFactory.Create(scan.BSSID);
+					        			if(wScan != null)
+					        			{
+					        				wScan.SetLevel(level);
+					        				count+=level;
+						        			weightedScans.add(wScan);
+						        			scannedAPs.put(wScan.Name(), wScan);
+					        			}
+				        	        }
+				        		}
+			        		}
 		        		}
+        			}
 		        		
-	        			Map(weightedScans, count);
-	        		}
-	        		else
-	        		{
-	        			textView.get().append("\nActivating Wi-Fi");
+	        		Map(weightedScans, count);
 	        		}
 	            }
-            }
         });
     }
     
@@ -145,26 +92,22 @@ public class ScanTask extends TimerTask{
 	*/
     private void Map(List<WeightedScan> scans, int count)
     {
-    	double[] loc = {0,0};
+    	float[] loc = {0,0};
     	if(scans.size() >= 0)
     	{			
 			//Calculations of position
 			for(WeightedScan scan : scans)
 			{	
-				loc[0] += scan.GetPos().get(0) * (double)scan.GetLevel()/(double)count;
-				loc[1] += scan.GetPos().get(1) * (double)scan.GetLevel()/(double)count;
-			}
-						
-
-			if(textView != null && textView.get() != null)
-			{
-				textView.get().append("\n\n( " + loc[0] + ",\n " + loc[1] + ")\n"); 
-			}
+				loc[0] += scan.GetPos().get(0) * (float)scan.GetLevel()/(float)count;
+				loc[1] += scan.GetPos().get(1) * (float)scan.GetLevel()/(float)count;
+			}			
     	}
     	else
     	{
 
     	}
+    	
+    	miD.UpdateLocation(loc[0], loc[1]);
     }
 }
 
