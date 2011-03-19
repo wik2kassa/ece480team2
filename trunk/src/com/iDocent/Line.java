059e75b4510fee18ab6387851617eb38d3a8d615
ghauls8@gmail.com <ghauls8@gmail.com@a65c081c-d945-0d18-fa3c-6faaeb5b76e5>
2011-03-19 18:49:15 +0000

    Added OpenGL stuff.  Old views have not been removed.

---
+++ trunk/src/com/iDocent/Line.java
@@ -0,0 +1,20 @@
+package com.iDocent;
+
+import java.nio.*;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class Line extends GraphicsObject{
+	
+	
+	public Line(){
+		
+	};
+
+	@Override
+	public void Draw(GL10 GL) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
