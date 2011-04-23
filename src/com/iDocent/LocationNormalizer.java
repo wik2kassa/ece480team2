bff40bf1f7fbd9ab24f4eb0fab4c355aeb8ec26b
ghauls8 <ghauls8@gmail.com>
2011-04-23 21:21:02 +0000

    Added header to all files for our team info

--- src/com/iDocent/LocationNormalizer.java
+++ src/com/iDocent/LocationNormalizer.java
@@ -1,3 +1,11 @@
+/*
+ * ECE 480 Spring 2011
+ * Team 2 Design Project
+ * Matt Gottshall
+ * Jake D'Onofrio
+ * Gordie Stein
+ * Andrew Kling
+ */
 package com.iDocent;
 
 //A static class to normalize all locations to work with the maps
