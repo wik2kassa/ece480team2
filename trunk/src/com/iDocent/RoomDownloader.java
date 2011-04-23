8c647920f7a254743ed54ae0c6f2d394bb2eabbf
ghauls8@gmail.com <ghauls8@gmail.com@a65c081c-d945-0d18-fa3c-6faaeb5b76e5>
2011-04-23 21:21:02 +0000

    Added header to all files for our team info

--- trunk/src/com/iDocent/RoomDownloader.java
+++ trunk/src/com/iDocent/RoomDownloader.java
@@ -1,3 +1,11 @@
+/*
+ * ECE 480 Spring 2011
+ * Team 2 Design Project
+ * Matt Gottshall
+ * Jake D'Onofrio
+ * Gordie Stein
+ * Andrew Kling
+ */
 package com.iDocent;
 
 import java.io.DataInputStream;
